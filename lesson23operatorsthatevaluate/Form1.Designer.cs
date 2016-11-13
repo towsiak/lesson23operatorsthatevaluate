589fe84173ee2acb0b487179438d1c43be6f7456
Tom Owsiak <towsiak@yahoo.com>
2016-11-13 02:03:05 -0500

    lesson 23 files

---
+++ lesson23operatorsthatevaluate/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace lesson23operatorsthatevaluate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(4, 13);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(870, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.button1.Location = new System.Drawing.Point(13, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 68);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add 2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.button2.Location = new System.Drawing.Point(13, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(355, 68);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Divide by 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
+            this.button3.Location = new System.Drawing.Point(13, 279);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(355, 68);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Subtract 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 632);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ lesson23operatorsthatevaluate/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lesson23operatorsthatevaluate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //+=2 has the effect of moving the track bar to the right by 2 on every click
+            //+=2 adds 2 to the current value of trackBar1.Value 
+            trackBar1.Value += 2;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //take the current value of the trackbar
+            //divide it by 2
+            //store that new, updated value back
+            trackBar1.Value /= 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //grab the current value of trackBar1.Value
+            //subtract 3 from it
+            //store back the updated value
+            trackBar1.Value -= 3;
+        }
+    }
+}
